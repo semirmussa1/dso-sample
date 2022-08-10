4fe6d10dbb9e712b5aa44b26627f09482e9cf6b4
semirmussa1 <91274900+semirmussa1@users.noreply.github.com>
2022-08-10 16:43:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-//const express = require('express')
-//const app = express()
-//app.get('/', function(request,result){
-    
-    // result.send('Hello World!! from ')
-    var name = prompt("Enter your name", "Enter name");
-    document.write("Hello " + name + "!");
-    //document.getElementById("demo").innerHTML = "Hello Connor!" + name;
-    console.log('hello world app is listening on port 3333')
-//})
+const express = require('express')
+const app = express()
+app.get('/', function(request,result){
+    result.send('Hello World. D2S!!')
+})
 app.listen(3333,"0.0.0.0",function(){
-   console.log('hello world app is listening on port 3333')
-
+    console.log('hello world app is listening on port 3333. D2S')
+})

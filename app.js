eeb42612e4c31fd9880f67a84c01ec20c1d243cb
semirmussa1 <s.mussa@accenture.com>
2022-08-10 15:09:46 +0100

    change for sonar

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
-app.get('/', function(request,result){
-	var name = prompt("Enter your name", "Enter name");
-    result.send('Hello World!! from ' + name)
-})
-app.listen(3333,"0.0.0.0",function(){
+//const express = require('express')
+//const app = express()
+//app.get('/', function(request,result){
+    
+    // result.send('Hello World!! from ')
+    var name = prompt("Enter your name", "Enter name");
+    document.write("Hello " + name + "!");
+    //document.getElementById("demo").innerHTML = "Hello Connor!" + name;
     console.log('hello world app is listening on port 3333')
+//})
+app.listen(3333,"0.0.0.0",function(){
+   console.log('hello world app is listening on port 3333')
 })

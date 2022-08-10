37690748bef1b37cc18be4a1b56a57a564c16aff
semirmussa1 <91274900+semirmussa1@users.noreply.github.com>
2022-08-10 13:41:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 app.get('/', function(request,result){
-    result.send('Hello World. from Gary!!')
+    result.send('Hello World. !!')
 })
 app.listen(3333,"0.0.0.0",function(){
-    console.log('hello world app is listening on port 3333. from Gary')
+    console.log('hello world app is listening on port 3333.')
 })
